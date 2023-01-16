9bd69e71c749ca9170b07d68438ab6d0bda35c7d
Alex Craig <Mr-Craig@users.noreply.github.com>
2023-01-16 16:09:36 +0000

    Frontend - Add Refresher to Server List

--- App/src/pages/serverList.tsx
+++ App/src/pages/serverList.tsx
@@ -1,4 +1,4 @@
-import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonButtons, IonBackButton, IonSearchbar, IonItem, IonLabel, IonImg } from "@ionic/react";
+import { IonApp, IonHeader, IonToolbar, IonTitle, IonContent, IonText, IonButtons, IonBackButton, IonSearchbar, IonItem, IonLabel, IonImg, IonRefresher, IonRefresherContent, RefresherEventDetail } from "@ionic/react";
 import { useEffect, useState } from "react";
 
 interface serverEntry 
@@ -16,13 +16,27 @@ const ServerList : React.FC = () => {
             console.log("Fetching Server List");
             fetch('https://lookup.acraig.tech/')
             .then((response) => response.json())
-            .then((data) => setServerList(data));
+            .then((data) => setServerList(data))
+            .catch((err) => {
+                console.log(err);
+            });
         }
     }, []);
 
     useEffect(() => {
         //console.log(searchTerm.length);
     }, [searchTerm]);
+
+    function handleRefresh(event: CustomEvent<RefresherEventDetail>) {
+        fetch('https://lookup.acraig.tech/')
+            .then((response) => response.json())
+            .then((data) => {
+                setServerList(data);
+                event.detail.complete();
+            }).catch((err) => {
+                console.log(err);
+            });
+    }
 
     return (
         <IonApp>
@@ -35,13 +49,16 @@ const ServerList : React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent>
+                <IonRefresher slot="fixed" pullFactor={0.5} pullMin={100} pullMax={200} onIonRefresh={handleRefresh}>
+                    <IonRefresherContent></IonRefresherContent>
+                </IonRefresher>
                 <IonSearchbar animated={true} placeholder="Server Name" showClearButton="focus" onIonChange={(event) => {
                     setSearchTerm(event.detail.value as string);
                 }}></IonSearchbar>
                 {serverList.filter((server : serverEntry) => { return server.serverIp.search(searchTerm) != -1; } ).map((server : serverEntry, i) => {
                     return (
                         <IonItem button key={i}>
-                            <IonLabel><img crossOrigin="anonymous" src={`https://countryflagsapi.com/png/${server.geolocation}`} height="11.2" width="16"/> {server.serverIp}</IonLabel>
+                            <IonLabel><img crossOrigin="anonymous" src={`https://flagcdn.com/w20/${server.geolocation === "" ? "gb" : server.geolocation.toLowerCase()}.png`} width="20"/> {server.serverIp}</IonLabel>
                         </IonItem>
                     )
                 })}
